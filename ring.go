0a2c5caae4c30ad65e04b6a7537e94ef33b7036f
onur1 <onur1@onurgunduz.com>
2023-02-04 05:02:53 +0100

    Use int

--- ring.go
+++ ring.go
@@ -3,18 +3,18 @@ package ring
 // A Ring represents a data structure that uses a single fixed-size buffer
 // as if it were connected end-to-end.
 type Ring[T any] struct {
-	size uint
-	mask uint
+	size int
+	mask int
 	buf  []T
 }
 
 // NewRing returns a new Ring with the given size which must be a power of 2.
-func NewRing[T any](size uint) *Ring[T] {
+func NewRing[T any](size int) *Ring[T] {
 	r := new(Ring[T])
 	if size > 0 && (size&(size-1) == 0) {
 		r.reset(size)
 	} else {
-		var n uint = 1
+		n := 1
 		for n < size {
 			n <<= 1
 		}
@@ -23,7 +23,7 @@ func NewRing[T any](size uint) *Ring[T] {
 	return r
 }
 
-func (c *Ring[T]) reset(size uint) {
+func (c *Ring[T]) reset(size int) {
 	*c = Ring[T]{
 		mask: size - 1,
 		size: size,
@@ -32,24 +32,24 @@ func (c *Ring[T]) reset(size uint) {
 }
 
 // Size returns the capacity of the internal buffer.
-func (c *Ring[T]) Size() uint {
+func (c *Ring[T]) Size() int {
 	return c.size
 }
 
 // Get returns the value at given index.
-func (c *Ring[T]) Get(index uint) T {
+func (c *Ring[T]) Get(index int) T {
 	return c.buf[index&c.mask]
 }
 
 // Put inserts a value at given index.
-func (c *Ring[T]) Put(index uint, val T) uint {
+func (c *Ring[T]) Put(index int, val T) int {
 	i := index & c.mask
 	c.buf[i] = val
 	return i
 }
 
 // Del deletes a value at given index and returns it.
-func (c *Ring[T]) Del(index uint) T {
+func (c *Ring[T]) Del(index int) T {
 	i := index & c.mask
 	val := c.buf[i]
 	c.buf[i] = *new(T)
